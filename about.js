6dafa1b64b8708daa2a53f48380001cb2441bf83
aschemily <aschemily@gmail.com>
2021-12-17 09:30:18 -0500

    part1,2,3

--- about.js
+++ about.js
@@ -1,11 +1,17 @@
 console.log("hello world");
 
+const catPhoto = document.getElementById('cat-photo')
 
+catPhoto.addEventListener('mouseover', ()=>{
+	//console.log('e is', e)
+	alert(`The kitty thinks you're doing a great job`)
+})
 
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	// console.log('form submit');
+	alert("Form has been submitted!")
 }
 
 
